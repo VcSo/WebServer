7e8c615618bfc04e9d7b8581ffd9769724ccc252
Vcvc <vcvckw@gmail.com>
2021-08-18 23:26:49 +0800

    pool

---
+++ ThreadPool/ThreadPool.hpp
@@ -0,0 +1,19 @@
+#ifndef WEBSERVER_THREADPOOL_HPP
+#define WEBSERVER_THREADPOOL_HPP
+
+template <class T>
+class ThreadPool
+{
+public:
+    ThreadPool(int num);
+
+private:
+
+};
+
+template <class T>
+ThreadPool<T>::ThreadPool(int num)
+{
+}
+
+#endif //WEBSERVER_THREADPOOL_HPP
