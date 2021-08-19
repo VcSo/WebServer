fc318313ee49bffeb692d2995550b621b45d9705
Vcvc <vcvckw@gmail.com>
2021-08-19 23:27:00 +0800

    timer

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -1,5 +1,40 @@
-//
-// Created by Vcvc on 2021/8/17.
-//
-
 #include "Timer.h"
+
+Timer::Timer()
+{
+
+}
+
+Timer::~Timer()
+{
+
+}
+
+int Timer::GetNextTick() {
+    tick();
+    size_t res = -1;
+    if(!heap.empty()) {
+        res = std::chrono::duration_cast<MS>(heap.front().expires - Clock::now()).count();
+        if(res < 0) { res = 0; }
+    }
+    return res;
+}
+
+void Timer::tick()
+{
+    if(heap.empty())
+    {
+        return;
+    }
+
+    while(!heap.empty())
+    {
+        TimerNode node = head.front();
+        if(std::chrono::duration_cast<MS>(node.expires - Clock::now()).count() > 0) {
+            break;
+        }
+
+        node.cb();
+        pop();
+    }
+}
--- Timer/Timer.h
+++ Timer/Timer.h
@@ -3,16 +3,39 @@
 
 #include <time.h>
 #include <unistd.h>
+#include <functional>
+#include <chrono>
+#include <vector>
+#include <unordered_map>
+
+typedef std::function<void()> TimeoutCallBack;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds MS;
+typedef Clock::time_point TimeStamp;
+
+struct TimerNode{
+    int id;
+    TimeStamp expires;
+    TimeoutCallBack cb;
+    bool operator<(const TimerNode& t) {
+        return expires < t.expires;
+    }
+};
 
 class Timer
 {
 public:
-    Timer()
-    {
+    Timer();
+    ~Timer();
 
-    }
+    void clear();
+    void tick();
+
+    int GetNextTick();
 
 private:
+    std::vector<TimerNode> heap;
+
 
 };
 
