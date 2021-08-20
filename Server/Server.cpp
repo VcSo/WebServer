fbced1d85b2c81989b3afa3315c6af6dc3f14021
Vcvc <vcvckw@gmail.com>
2021-08-20 23:22:35 +0800

    server

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,168 +1,32 @@
 #include "Server.h"
 
-WebServer::WebServer(int port, int et, int timeout, bool optlinger,
-          int sql_port, std::string sql_username, std::string sql_user_password, std::string sql_database,
-          int sqlconn_num, int thread_num, bool log_flag, int log_level, int log_queue_size)
-          : m_port(port), m_et(et), m_timeout(timeout), m_optlinger(optlinger),
-            m_sql_port(sql_port), m_sql_username(sql_username), m_sql_password(sql_user_password), m_sql_database(sql_database),
-            m_sql_conn_num(sqlconn_num), m_threadpool(new ThreadPool<WebServer>(thread_num)), m_timer(new Timer()), m_epoll(new Epoll())
+Server::Server(int port, std::string host, std::string username, std::string password, std::string database,
+                bool uselog, int linger, int mode, int sqlnum, int threadnum, int actor, int logmode)
+                : m_port(port), m_host(host), m_username(username), m_password(password), m_database(database),
+                    m_uselog(uselog), m_linger(linger), et(mode), m_sql_num(sqlnum), m_thead_num(threadnum), m_actor(actor), m_log_mode(logmode)
 {
-    srcdir = getcwd(nullptr, 256);
-    assert(srcdir);
-    strncat(srcdir,  "/resources/", 16);
-    std::cout << srcdir << std::endl;
-
-    Httpconn::srcDir = srcdir;
-    Httpconn::userCount = 0;
-
-    SqlConnPool::Instance()->init("localhost", m_sql_port, m_sql_username, m_sql_password, m_sql_database, m_sql_conn_num);
-    InitEventMode(et);
-    if(!InitSocket())
-    {
-        isClose = true;
-    }
-    //if(openlog)
-    {
-
-    }
 
 }
 
-void WebServer::InitEventMode(int trigMode) {
-    listenEvent = EPOLLRDHUP;
-    connEvent = EPOLLONESHOT | EPOLLRDHUP;
-    switch (trigMode)
-    {
-        case 0:
-            break;
-        case 1:
-            connEvent |= EPOLLET;
-            break;
-        case 2:
-            listenEvent |= EPOLLET;
-            break;
-        case 3:
-            listenEvent |= EPOLLET;
-            connEvent |= EPOLLET;
-            break;
-        default:
-            listenEvent |= EPOLLET;
-            connEvent |= EPOLLET;
-            break;
-    }
-    Httpconn::isET = (connEvent & EPOLLET);
+Server::~Server()
+{}
+
+void Server::setsql()
+{
+    m_consql = connSql::get_instance();
+    m_consql->init(m_host, m_username, m_password, m_database, m_sql_num);
 }
 
-bool WebServer::InitSocket()
+void Server::setlog(std::string path)
 {
-    int ret;
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(m_port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    struct linger optLinger = {0};
-    if(m_optlinger)
+    m_log_path = path;
+    if(m_uselog)
     {
-        //优雅关闭
-        optLinger.l_linger = 1;
-        optLinger.l_onoff = 1;
-    }
-
-    if(listenfd < 0)
-    {
-        //log
-        std::cout << "init sockfd error" << std::endl;
-        close(listenfd);
-        return false;
-    }
-
-    int reuse = 1;
-    ret = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void*) &reuse, sizeof(reuse));
-    if(ret == -1)
-    {
-        //log
-        close(listenfd);
-        return false;
-    }
-
-    ret = bind(listenfd, (struct sockaddr*) &addr, sizeof(addr));
-    if(ret < 0)
-    {
-        //log
-        std::cout << "sockfd bind error, port = " << m_port << std::endl;
-        close(listenfd);
-        return false;
-    }
-
-    ret = listen(listenfd, 10);
-    if(ret < 0)
-    {
-        //log
-        std::cout << "listen sockfd error, port = " << m_port << std::endl;
-        close(listenfd);
-        return false;
-    }
-
-    ret = epoller->AddFd(listenfd, listenEvent | EPOLLIN);
-    if(ret == 0)
-    {
-        //log
-        close(listenfd);
-        return false;
-    }
-
-    setNonblock(listenfd);
-}
-
-void WebServer::setNonblock(int fd)
-{
-    assert(fd > 0);
-    return fcntl(fd, F_SETFL, fcntl(fd, F_GETFD, 0) | O_NONBLOCK);
-}
-
-
-
-void WebServer::Start()
-{
-    int timeMS = -1;
-    if(!isClose)
-    {
-        //LOG_INFO("========== Server start ==========");
-    }
-
-    while(!isClose)
-    {
-        if(timeMS > 0)
+        if(m_log_mode == 1)
         {
-            timeMS = timer->GetNextTick();
-        }
-        int eventCnt = epoller->Wait(timeMS);
-
-        for(int i = 0; i < eventCnt; i++) {
-            /* 处理事件 */
-            int fd = epoller->GetEventFd(i);
-            uint32_t events = epoller->GetEvents(i);
-            if(fd == listenfd) {
-                DealListen_();
-            }
-            else if(events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
-                assert(users_.count(fd) > 0);
-                CloseConn_(&users_[fd]);
-            }
-            else if(events & EPOLLIN) {
-                assert(users_.count(fd) > 0);
-                DealRead_(&users_[fd]);
-            }
-            else if(events & EPOLLOUT) {
-                assert(users_.count(fd) > 0);
-                DealWrite_(&users_[fd]);
-            } else {
-                //log
-                //LOG_ERROR("Unexpected event");
-            }
+            Log::get_instance()->init(m_log_path, m_uselog, 2000, 800000, 800);
+        } else{
+            Log::get_instance()->init(m_log_path, m_uselog, 2000, 800000, 0);
         }
     }
 }
--- Server/Server.h
+++ Server/Server.h
@@ -1,62 +1,37 @@
 #ifndef WEBSERVER_SERVER_H
 #define WEBSERVER_SERVER_H
 
-#include <iostream>
-#include <unistd.h>
-#include <cstring>
-#include <time.h>
-#include <mysql/mysql.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <memory>
-#include <fcntl.h>
+#include <string>
+#include "../Sql/connSql.h"
 
-#include "../ThreadPool/ThreadPool.hpp"
-#include "../Timer/Timer.h"
-#include "./Epoll.h"
-#include "../Http/Httpconn.h"
-#include "../Sql/Sqlconn.h"
-
-class WebServer {
+class Server {
 public:
-    WebServer(int port, int et, int timeout, bool optlinger,
-              int sql_port, std::string sql_username, std::string sql_user_password, std::string sql_database,
-              int conn_num, int thread_num, bool log_flag, int log_level, int log_queue_size);
+    Server(int port, std::string m_host, std::string username, std::string password, std::string database,
+                   bool uselog, int linger, int mode, int sqlnum, int threadnum, int actor, int logmode);
+    ~Server();
 
-    void InitEventMode(int trigMode);
-    void setNonblock(int fd);
-    void Start();
+    void setsql();
+    void setlog(std::string path);
+    //void Start();
 
-    bool InitSocket();
 
 private:
-    char *srcdir;
-
     int m_port;
-    int m_et;
-    int m_sql_port;
-    int m_sql_conn_num;
-    int listenfd;
+    int m_linger;
+    int et;
+    int m_sql_num;
+    int m_thead_num;
+    int m_actor;
+    int m_log_mode;
 
-    bool m_optlinger;
-    bool isClose;
+    std::string m_host;
+    std::string m_username;
+    std::string m_password;
+    std::string m_database;
+    std::string m_log_path;
 
-    uint32_t listenEvent;
-    uint32_t connEvent;
-    time_t m_timeout;
-
-    std::string m_sql_username;
-    std::string m_sql_password;
-    std::string m_sql_database;
-
-private:
-    ThreadPool<WebServer> *m_threadpool;
-    Timer *m_timer;
-    Epoll *m_epoll;
-    std::unique_ptr<Epoll> epoller;
-    std::unique_ptr<Timer> timer;
+    connSql *m_consql;
+    bool m_uselog;
 };
-
 
 #endif //WEBSERVER_SERVER_H
