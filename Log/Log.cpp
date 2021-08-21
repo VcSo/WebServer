c4ecb17599bc40360cb520d30ff37c20c8dfca3a
Vcvc <vcvckw@gmail.com>
2021-08-21 23:04:51 +0800

    log

--- Log/Log.cpp
+++ Log/Log.cpp
@@ -9,6 +9,19 @@ void Log::init(std::string path, bool uselog, int log_buf_size, int split_line, 
     if(max_queue_size > 0)
     {
         m_is_async = true;
-
+        pthread_t tid;
+        pthread_create(&tid, NULL, flush_log_threaad, NULL);
     }
+}
+
+void *Log::flush_log_threaad(void *args)
+{
+    Log::get_instance()->async_write_log();
+}
+
+void *Log::async_write_log()
+{
+    std::string single_log;
+    while()
+
 }
--- Log/Log.h
+++ Log/Log.h
@@ -9,11 +9,13 @@
 class Log {
 public:
     static Log *get_instance();
+    static void *flush_log_threaad(void *args);
     void init(std::string path, bool uselog, int log_buf_size, int split_line, int max_queue_size);
 
 private:
     Log();
     ~Log();
+    void *async_write_log();
 
     std::string m_logfile_name;
     std::string m_fileline;
@@ -21,6 +23,8 @@ private:
     std::ofstream writelog;
 
     bool m_is_async;
+
+    std::queue<std::string> m_log_que;
 };
 
 
