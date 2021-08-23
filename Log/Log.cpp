5b49249ff56a10432075808934423bec0ce99d96
Vcvc <vcvckw@gmail.com>
2021-08-23 17:55:38 +0800

    log

--- Log/Log.cpp
+++ Log/Log.cpp
@@ -22,6 +22,10 @@ void *Log::flush_log_threaad(void *args)
 void *Log::async_write_log()
 {
     std::string single_log;
-    while()
-
+    while (m_log_que->pop(single_log))
+    {
+        m_mutex.lock();
+        fputs(single_log.c_str(), m_fp);
+        m_mutex.unlock();
+    }
 }
