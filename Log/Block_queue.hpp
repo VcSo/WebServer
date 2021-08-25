b7a1822a33556e8f149bd79f60015fc72c0770bf
Vcvc <vcvckw@gmail.com>
2021-08-25 21:52:22 +0800

    que

--- Log/Block_queue.hpp
+++ Log/Block_queue.hpp
@@ -10,10 +10,11 @@ template <typename T>
 class block_queue
 {
 public:
-    block_queue();
     block_queue(int max_size);
     ~block_queue();
 
+    void clear();
+    bool full();
     bool push(T content);
     bool pop(T &content);
 
@@ -32,11 +33,10 @@ template <typename T>
 block_queue<T>::block_queue(int max_size)
 {
     m_max_size = max_size;
-    m_array = new T[max_size];
+    m_array = new T[m_max_size];
     m_size = 0;
     m_front = -1;
     m_back = -1;
-
 }
 
 template <typename T>
@@ -48,6 +48,50 @@ block_queue<T>::~block_queue()
         delete[] m_array;
     }
     m_mutex.unlock();
+}
+
+template <typename T>
+void block_queue<T>::clear()
+{
+    m_mutex.lock();
+    m_size = 0;
+    m_front = -1;
+    m_back = -1;
+
+    m_mutex.unlock();
+}
+
+template <typename T>
+bool block_queue<T>::full()
+{
+    m_mutex.lock();
+    if(m_size >= m_max_size)
+    {
+        m_mutex.unlock();
+        return true;
+    }
+
+    m_mutex.unlock();
+    return false;
+}
+
+template <typename T>
+bool block_queue<T>::push(T content)
+{
+    m_mutex.lock();
+    if(m_size >= m_max_size)
+    {
+        m_mutex.unlock();
+        return false;
+    }
+    m_back = (m_back + 1) % m_max_size;
+    m_array[m_back] = content;
+    m_size++;
+
+    m_cond.broadcast();
+    m_mutex.unlock();
+
+    return true;
 }
 
 template <typename T>
@@ -70,7 +114,5 @@ bool block_queue<T>::pop(T &content)
 
     return true;
 }
-
-
 
 #endif //WEBSERVER_BLOCK_QUEUE_HPP
