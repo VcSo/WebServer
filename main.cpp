20c47e6888decb01e43b559d9d323bb69305ab6f
Vcvc <vcvckw@gmail.com>
2021-09-01 18:37:50 +0800

    init

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     }
 
     Server server(port, "localhost", "root", "vcvc", "server",
-                    true, 1, 1, 8, 10, 1, 1); //启用 友善关闭 lt/et sql thread actor 异步
+                    1, 1, 1, 8, 10, 1, 1); //启用 友善关闭 lt/et sql thread actor 异步
 
     //server.setsql();
     server.setlog("./savelog/");
