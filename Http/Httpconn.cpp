b80503205b7e7c62966d7c6b6f08c19cef83137b
Vcvc <vcvckw@gmail.com>
2021-09-04 21:02:17 +0800

    http

--- Http/Httpconn.cpp
+++ Http/Httpconn.cpp
@@ -2,3 +2,84 @@
 
 int Http::m_user_count = 0;
 int Http::m_epollfd = -1;
+
+Http::Http()
+{
+
+}
+
+Http::~Http()
+{
+
+}
+
+void Http::init()
+{
+    mysql = nullptr;
+    bytes_have_end = 0;
+    bytes_to_end = 0;
+    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_linger = false;
+    m_method = GET;
+    m_url = nullptr;
+    m_version = nullptr;
+    m_content_length = 0;
+    m_host = nullptr;
+    m_start_line = 0;
+    m_checked_idx = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+    cgi = 0;
+    m_state = 0;
+    timer_flag = 0;
+    improv = 0;
+
+    memset(m_read_buf, '\0', READ_BUFFER_SIZE);
+    memset(m_write_buf, '\0', WRITE_BUFFER_SIZE);
+    memset(m_real_file, '\0', FILENAME_LEN);
+}
+
+//对文件描述符设置非阻塞
+int setnonblocking(int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+//将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
+void addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
+{
+    epoll_event event;
+    event.data.fd = fd;
+
+    if (1 == TRIGMode)
+        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+    else
+        event.events = EPOLLIN | EPOLLRDHUP;
+
+    if (one_shot)
+        event.events |= EPOLLONESHOT;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+void Http::init(int sockfd, const struct sockaddr_in &addr, char *m_root, int conn_mode, int close_log,
+                    std::string username, std::string password, std::string database)
+{
+    m_sockfd = sockfd;
+    m_addr = addr;
+    addfd(m_epollfd, sockfd, true, conn_mode);
+    ++m_user_count;
+
+    doc_root = m_root;
+    m_conn_mode = conn_mode;
+    m_close_log = close_log;
+
+    sql_username = username;
+    sql_password = password;
+    sql_database = database;
+
+    init();
+}
--- Http/Httpconn.h
+++ Http/Httpconn.h
@@ -1,20 +1,110 @@
 #ifndef WEBSERVER_HTTPCONN_H
 #define WEBSERVER_HTTPCONN_H
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <mysql/mysql.h>
+
 #include "../Log/Log.h"
 #include "../Timer/Timer.h"
 
 class Http {
 public:
+    enum METHOD
+    {
+        GET = 0,
+        POST,
+        HEAD,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT,
+        PATH
+    };
+
+    enum HTTP_CODE
+    {
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+
+    enum CHECK_STATE
+    {
+        CHECK_STATE_REQUESTLINE = 0,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
+
+    enum LINE_STATUS
+    {
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
+
+    static const int FILENAME_LEN = 200;
+    static const int READ_BUFFER_SIZE = 2048;
+    static const int WRITE_BUFFER_SIZE = 1024;
+
     Http();
     ~Http();
 
+    void init(int sockfd, const struct sockaddr_in &addr, char *m_root, int conn_mode, int close_log,
+                std::string username, std::string password, std::string database);
+
+public:
     static int m_epollfd;
     static int m_user_count;
 
     int m_state;
     int improv;
+    MYSQL *mysql;
 
+private:
+    void init();
+
+    int m_sockfd;
+    int m_conn_mode;
+    int m_close_log;
+    int bytes_to_end;
+    int bytes_have_end;
+    int m_content_length;
+    int m_start_line;
+    int m_checked_idx;
+    int m_read_idx;
+    int m_write_idx;
+    int cgi;
+    int timer_flag;
+
+    char *doc_root;
+    char *m_url;
+    char *m_version;
+    char *m_host;
+    char m_read_buf[READ_BUFFER_SIZE];
+    char m_write_buf[WRITE_BUFFER_SIZE];
+    char m_real_file[FILENAME_LEN];
+
+    std::string sql_username;
+    std::string sql_password;
+    std::string sql_database;
+
+    bool m_linger;
+
+    METHOD m_method;
+    CHECK_STATE m_check_state;
+    struct sockaddr_in m_addr;
+    std::map<std::string, std::string> m_users;
 };
 
 
