fa9ff996a33b92abd8560aabb65730f5e7f77770
Vcvc <vcvckw@gmail.com>
2021-09-09 23:06:15 +0800

    http

---
+++ Http/Httpconn.h
@@ -0,0 +1,23 @@
+#ifndef HTTPCONN_H
+#define HTTPCONN_H
+
+#include <map>
+#include <string>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#include "../Log/Log.h"
+#include "../Lock/Locker.h"
+
+class Http
+{
+public:
+
+private:
+
+};
+
+#endif
