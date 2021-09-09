306ab4d863e9bc5e16bdb17aebeb9c324e268d4e
Vcvc <vcvckw@gmail.com>
2021-09-09 10:04:02 +0800

    server

---
+++ Server/Server.cpp
@@ -0,0 +1,35 @@
+#include "Server.h"
+
+Server::Server() {}
+
+Server::~Server() {}
+
+Server::Server(int port, std::string localhost, std::string sql_username, std::string sql_password, std::string sql_database,
+                    int close_log, int lingermode, int et, int sql_threadnum, int threadnum, int actor_mode, int async)
+                    : m_port(port), m_localhost(localhost), m_sql_username(sql_username), m_sql_password(sql_password), m_sql_database(sql_database),
+                    m_close_log(close_log), m_lingermode(lingermode), m_et(et), m_sqlthreadnum(sql_threadnum), m_threadnum(threadnum), m_actor_mode(actor_mode), m_async(async)
+
+{
+    char server_path[256];
+    getcwd(server_path, 256);
+    char root[] = "/root";
+    m_root = (char *)malloc(strlen(server_path) + strlen(root) + 1);
+    strcpy(m_root, server_path);
+    strcat(m_root, root);
+}
+
+void Server::set_log(std::string path)
+{
+    if(m_close_log == true)
+    {
+        if(m_async == 1)
+        {
+            Log::get_instance()->init(path, m_close_log, 2000, 800000, 800);
+        }
+        else
+        {
+            Log::get_instance()->init(path, m_close_log, 2000, 800000, 0);
+        }
+    }
+}
+
--- Server/Server.h
+++ Server/Server.h
@@ -5,18 +5,34 @@
 
 #include "../Log/Log.h"
 
-namespace vc
-{
-    class Server
-    {
-    public:
-        Server();
-        ~Server();
-        Server(int port, )
+class Server {
+public:
+    Server();
 
-    private:
+    ~Server();
 
-    };
-}
+    Server(int port, std::string localhost, std::string sql_username, std::string sql_password, std::string sql_database,
+                   int close_log, int lingermode, int et, int sql_threadnum, int threadnum, int actor_mode, int async);
 
-#endif SSERVER_H
+    void set_log(std::string path);
+
+
+private:
+    std::string m_localhost;
+    std::string m_sql_username;
+    std::string m_sql_password;
+    std::string m_sql_database;
+
+    char *m_root;
+
+    bool m_close_log;
+    int m_port;
+    int m_lingermode;
+    int m_et;
+    int m_sqlthreadnum;
+    int m_threadnum;
+    int m_actor_mode;
+    int m_async;
+};
+
+#endif
