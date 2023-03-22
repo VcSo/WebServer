a8ab1ba7689fac607f1ce49b54f96d20a612736c
Vc <vcvckw@gmail.com>
2023-03-22 23:45:43 +0800

    init

--- main.cpp
+++ main.cpp
@@ -11,7 +11,17 @@ int main(int argc, char **argv)
         port = atoi(argv[1]);
     }
 
-    Server server(port, "localhost", "root", "sqlpassword", "serverm",
+    std::cout << "http://101.132.159.69:" << port << std::endl;
+
+/*
+0，表示使用LT + LT
+1，表示使用LT + ET
+2，表示使用ET + LT
+3，表示使用ET + ET
+
+actor 1 = re 0=pr
+*/
+    Server server(port, "localhost", "root", "zxz456123", "serverm",
                   true, 1, 1, 8, 10, 1, 1); //启用 友善关闭 lt/et sql thread actor 异步
 
     server.set_log("./Savelog/");
@@ -25,3 +35,13 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+//create table stu(
+//        id int(10) not null auto_increment primary key,
+//        nickname char(16) default null,
+//        name char(20) not null,
+//        age int(2) default null,
+//        sex char(1) not null,
+//        phone int(11) not null
+//        create_time datetime
+//        )ENGINE=InnoDB default charset=utf8;
