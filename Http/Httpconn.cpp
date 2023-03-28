cc56eed8d524cbf114bf2aa139939aff2f7b47e8
Vc <vcvckw@gmail.com>
2023-03-28 23:45:19 +0800

    http

--- Http/Httpconn.cpp
+++ Http/Httpconn.cpp
@@ -116,7 +116,7 @@ void Http::init_mysqlresult(ConnSql *m_sql)
     connectionRAII mysqlcon(&mysql, m_sql);
 
     //在user表中检索username，passwd数据，浏览器端输入
-    if (mysql_query(mysql, "SELECT username,passwd FROM user"))
+    if (mysql_query(mysql, "SELECT username,password FROM user"))
     {
         LOG_ERROR("SELECT error:%s\n", mysql_error(mysql));
     }
@@ -620,12 +620,19 @@ Http::HTTP_CODE Http::do_request()
             //如果是注册，先检测数据库中是否有重名的
             //没有重名的，进行增加数据
             char *sql_insert = (char *)malloc(sizeof(char) * 200);
-            strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
+//            if(users.empty() == true)
+//            {
+//                strcpy(sql_insert, "INSERT INTO user(username, password,create_time) VALUES(1,");
+//            }
+//            else
+            {
+                strcpy(sql_insert, "INSERT INTO user(username, password,create_time) VALUES(");
+            }
             strcat(sql_insert, "'");
             strcat(sql_insert, name);
             strcat(sql_insert, "', '");
             strcat(sql_insert, password);
-            strcat(sql_insert, "')");
+            strcat(sql_insert, "',now())");
 
             if (users.find(name) == users.end())
             {
