0afa468efec1fa4585fe513b8803df1f4c02b651
Vc <vcvckw@gmail.com>
2023-04-07 01:54:06 +0800

    server

--- Server/Server.h
+++ Server/Server.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <netinet/in.h>
 
 #include "../Log/Log.h"
 #include "../Sql/connSql.h"
@@ -19,7 +20,7 @@ public:
     Server();
     ~Server();
 
-    Server(int port, std::string localhost, std::string sql_username, std::string sql_password, std::string sql_database,
+    Server(std::string ip, int port, std::string localhost, std::string sql_username, std::string sql_password, std::string sql_database,
                    bool close_log, int lingermode, int et, int sql_threadnum, int threadnum, int actor_mode, int async);
 
     void Start();
@@ -38,6 +39,7 @@ public:
     bool dealwithsignal(bool& timeout, bool& stop_server);
 
 private:
+    std::string m_ip;
     std::string m_localhost;
     std::string m_sql_username;
     std::string m_sql_password;
