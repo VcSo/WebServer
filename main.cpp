fcf228e6fbffd7b619e7d4a144c26604a7c5ca70
Vc <vcvckw@gmail.com>
2023-04-07 01:54:36 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,17 +1,92 @@
 #include <iostream>
+#include <getopt.h>
 
 #include "./Server/Server.h"
+
+static struct option opts[] = {
+        {"ip", required_argument, NULL, 'i'},
+        {"port", required_argument, NULL, 'p'},
+        {"sql_username", required_argument, NULL, 'u'},
+        {"sql_passwrod", required_argument, NULL, 'w'},
+        {"sql_database", required_argument, NULL, 'd'},
+        {"use_log", required_argument, NULL, 'g'},
+        {"lingermode", required_argument, NULL, 'l'},
+        {"et", required_argument, NULL, 'e'},
+        {"sql_threadnum", required_argument, NULL, 's'},
+        {"thread_num", required_argument, NULL, 't'},
+        {"actor_mode", required_argument, NULL, 'a'},
+        {"async", required_argument, NULL, 'c'},
+        {0, 0, 0, 0}
+};
 
 int main(int argc, char **argv)
 {
     std::cout << "main start" << std::endl;
+    std::string ip = "";
     int port = 20999;
-    if(argc > 2)
-    {
-        port = atoi(argv[1]);
-    }
+    std::string sql_username = "root";
+    std::string sql_password = "Aa1248800211";
+    std::string sql_database = "serverm";
+    bool use_log = true;
+    int lingermode = 1;
+    int et = 0;
+    int sql_threadnum = 8;
+    int thread_num = 10;
+    int actor_mode = 0;
+    int async = 1;
 
-    std::cout << "http://101.132.159.69:" << port << std::endl;
+    int opt = 0;
+    int optionindex = 0;
+
+//  ip, port, sql_username, sql_password, sql_database, use_log, lingermode, et, sqlthreadnum, threadnum, actor_mode, async
+    while((opt = getopt_long(argc, argv, "i:p:u:w:d:g:l:e:s:t:a:c:", opts, &optionindex)) != -1)
+    {
+        switch(opt)
+        {
+            case 'i':
+                ip = optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);;
+                break;
+            case 'u':
+                sql_username = optarg;
+                break;
+            case 'w':
+                sql_password = optarg;
+                break;
+            case 'd':
+                sql_database = optarg;
+                break;
+            case 'g':
+                if(*optarg == '1')
+                    use_log = true;
+                else
+                    use_log = false;
+                break;
+            case 'l':
+                lingermode = atoi(optarg);
+                break;
+            case 'e':
+                et = atoi(optarg);
+                break;
+            case 's':
+                sql_threadnum = atoi(optarg);
+                break;
+            case 't':
+                thread_num = atoi(optarg);
+                break;
+            case 'a':
+                actor_mode = atoi(optarg);
+                break;
+            case 'c':
+                actor_mode = atoi(optarg);
+                break;
+            default:
+                ;
+        }
+    }
+    std::cout << "http://" << ip << ":" << port << std::endl;
 
 /*
 0，表示使用LT + LT
@@ -19,10 +94,11 @@ int main(int argc, char **argv)
 2，表示使用ET + LT
 3，表示使用ET + ET
 
-actor 1 = re 0=pr
+actor 1 = re 0=pro
+async异步写日志
 */
-    Server server(port, "localhost", "root", "zxz456123", "serverm",
-                  true, 1, 1, 8, 10, 1, 1); //启用 友善关闭 lt/et sql thread actor 异步
+    Server server(ip, port, "localhost", sql_username, sql_password, sql_database,
+                  use_log, lingermode, et, sql_threadnum, thread_num, actor_mode, async); //日志 友善关闭 lt/et sql thread actor 异步
 
     server.set_log("./Savelog/");
     server.setsql();
@@ -35,13 +111,3 @@ actor 1 = re 0=pr
 
     return 0;
 }
-
-//create table stu(
-//        id int(10) not null auto_increment primary key,
-//        nickname char(16) default null,
-//        name char(20) not null,
-//        age int(2) default null,
-//        sex char(1) not null,
-//        phone int(11) not null
-//        create_time datetime
-//        )ENGINE=InnoDB default charset=utf8;
