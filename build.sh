b6a811e85599137de12fc7f91f39401fcdfe6ba8
Vc <vcvckw@gmail.com>
2023-05-19 03:59:06 +0800

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-rm -rf ./build
-mkdir build && cp -r ./resources/ ./build
-cd build
+if [ -d "./build" ]; then
+  cd build
+  rm CMakeCache.txt Makefile serverone cmake_install.cmake
+  rm -rf CMakeFiles
+else
+  mkdir build && cp -r ./resources/ ./build
+  cd build
+fi
 cmake ..
 make -j4
 ./serverone
